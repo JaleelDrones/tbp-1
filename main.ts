a712225465238c8c1093b56f69be31671eef96b1
JaleelDrones <100054244+JaleelDrones@users.noreply.github.com>
2022-02-19 19:41:36 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+let firstName = game.askForString("What is your first name")
+let Age = game.askForNumber("How old are you", 2)
+let favoriteFood = game.askForString("What is your favorite food")
+let mySprite = sprites.create(img`
+    . . 4 4 4 . . . . 4 4 4 . . . . 
+    . 4 5 5 5 e . . e 5 5 5 4 . . . 
+    4 5 5 5 5 5 e e 5 5 5 5 5 4 . . 
+    4 5 5 4 4 5 5 5 5 4 4 5 5 4 . . 
+    e 5 4 4 5 5 5 5 5 5 4 4 5 e . . 
+    . e e 5 5 5 5 5 5 5 5 e e . . . 
+    . . e 5 f 5 5 5 5 f 5 e . . . . 
+    . . f 5 5 5 4 4 5 5 5 f . . f f 
+    . . f 4 5 5 f f 5 5 6 f . f 5 f 
+    . . . f 6 6 6 6 6 6 4 4 f 5 5 f 
+    . . . f 4 5 5 5 5 5 5 4 4 5 f . 
+    . . . f 5 5 5 5 5 4 5 5 f f . . 
+    . . . f 5 f f f 5 f f 5 f . . . 
+    . . . f f . . f f . . f f . . . 
+    `, SpriteKind.Player)
+mySprite.sayText("The" + firstName.substr(0, 3) + "ster" + "ate" + Age + "" + favoriteFood + "" + "for breakfast")
